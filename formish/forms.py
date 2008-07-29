3159fb61764216579867010f50a8833e743bbd35
Tim Parkin <tim.parkin@jdi-associates.com>
2008-07-29 16:32:23 +0100

    Got cssname working across all widgets and field.html

--- formish/forms.py
+++ formish/forms.py
@@ -56,7 +56,7 @@ class Field(object):
 
     @property
     def cssname(self):
-        return '%s-%s'%field.form.name, '-'.join(field.name.split('.'))
+        return '%s-%s'%(self.form.name, '-'.join(self.name.split('.')))
     
             
     @property
@@ -184,25 +184,20 @@ class Form(object):
                 form_item.widget = widget
 
     def addAction(self, callback, name="submit", label=None):
-        print 'adding action'
         if self.actions is None:
             self.actions = []
         if name in [action.name for action in self.actions]:
             raise ValueError('Action with name %r already exists.' % name)
         self.actions.append( Action(callback, name, label) )              
-        print 'action added',self.actions
 
     def action(self):
         if len(self.actions)==0:
             raise NoActionError('The form does not have any actions')
         for action in self.actions:
-            print 'trying to find %s in %r'%(action.name, self.requestData.keys())
             if action.name in self.requestData.keys():
                 return action.callback(self)
         return self.actions[0].callback(self)
             
-            
-        
         
     def __call__(self):
         return literal(render(self.request, "formish/form.html", {'form': self}))
