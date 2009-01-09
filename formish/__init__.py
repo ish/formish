234e50c105b7d7d1e77e1c392200668891130840
Matt Goodall <matt@jdi-associates.com>
2009-01-09 16:28:55 +0000

    Add missing exception to package-level exports.

--- formish/__init__.py
+++ formish/__init__.py
@@ -2,7 +2,7 @@
 Base package to import top level modules
 """
 from formish.forms import Form
-from formish.validation import FieldError, FormError, FormishError
+from formish.validation import FieldError, FormError, FormishError, NoActionError
 from formish.widgets import *
 from formish.util import form_in_request
 
