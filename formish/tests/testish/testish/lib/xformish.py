c5158020475e62d7e8b86a613a02c0a659038f88
Matt Goodall <matt@jdi-associates.com>
2009-01-09 16:31:33 +0000

    Fix custom widget to raise correct exception type.

--- formish/tests/testish/testish/lib/xformish.py
+++ formish/tests/testish/testish/lib/xformish.py
@@ -3,6 +3,7 @@ General purpose formish extensions.
 """
 
 from formish import validation, widgets, Form
+from convertish.convert import ConvertError
 
 
 class DateParts(widgets.DateParts):
@@ -27,15 +28,15 @@ class ApproximateDateParts(widgets.DateParts):
         # Collect all the parts from the request.
         parts = (data['year'][0].strip(), data['month'][0], data['day'][0])
         if not parts[0] and (parts[1] or parts[2]):
-            raise validation.FieldValidationError("Invalid date")
+            raise ConvertError("Invalid date")
         elif not parts[1] and parts[2]:
-            raise validation.FieldValidationError("Invalid date")
+            raise ConvertError("Invalid date")
         # Discard the unspecified parts
         parts = [p for p in parts if p]
         # Ensure they're all integers (don't record the result, we don't care).
         try:
             [int(p) for p in parts]
         except ValueError:
-            raise validation.FieldValidationError("Invalid date")
+            raise ConvertError("Invalid date")
         return '-'.join(parts)
 
