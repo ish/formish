ef3d17010d2bb2ca5710709331c6c1fbb6eb857a
Matt Goodall <matt@jdi-associates.com>
2009-01-14 12:00:28 +0000

    Allow all tests to be run in a single browser session or each test in a separate browser session.
    In reality, the forms and tests are stateless so running in a single
    browser session *should* have no impact. However, it's sometimes
    convenient, especially during development, to be able to select a
    particular test to run.

--- formish/tests/testish/testish/lib/test_func.py
+++ formish/tests/testish/testish/lib/test_func.py
@@ -2,11 +2,34 @@ from testish.lib.base import TestCase
 from testish.lib import forms
 
 
-class Test(TestCase):
+class TestTogether(TestCase):
+    """
+    Run all tests in a single browser session.
+    """
 
     def test_func(self):
         sel = self.selenium
         for attr in dir(forms):
             if attr.startswith('functest_'):
-                getattr(forms,attr)(self,sel)
+                getattr(forms,attr)(self)
+
+
+class TestSeparately(TestCase):
+    """
+    Run each test in a separate browser session.
+
+    Test methods are dynamically added to this class by replacing the functest_
+    prefix with test_ and attaching the method to the class (see below).
+    """
+    pass
+
+
+# Dynamically add test methods to the TestSeparately class. Yep, this is a
+# truly horrible hack.
+for attr in dir(forms):
+    if attr.startswith('functest_'):
+        func_name = 'test_%s'%attr[9:]
+        func = getattr(forms, attr)
+        func.__name__ = func_name
+        setattr(TestSeparately, func_name, func)
 
