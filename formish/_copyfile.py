d71b295420a0d8599ab3b564c749e3559f1e5186
Matt Goodall <matt@jdi-associates.com>
2009-03-06 10:48:27 +0000

    Add a more cache-friendly shutil.copyfileobj implementation.
    At the moment, uploaded files are always copied to a temporary file
    cache in case validation fails. (Formish probably shouldn't force this
    behaviour actually, it's not always needed).
    
    On Linux, if a very large file is uploaded the file in the request is
    cached. Formish copies it and the copy gets cached too resulting in a
    lot of RAM being used to cache the upload.
    
    Now, as it happens, we're never going to access the request's file once
    it's been copied so we can advise the (Posix-compliant) OS to drop the
    request's file from the cache.
    
    Note: the more efficient copy is only used if the python-advise package
    is installed.

---
+++ formish/_copyfile.py
@@ -0,0 +1,44 @@
+__all__ = ['copyfileobj']
+
+import shutil
+
+
+try:
+    from fadvise import posix_fadvise, POSIX_FADV_SEQUENTIAL, POSIX_FADV_DONTNEED
+
+    def copyfileobj(fsrc, fdst, length=16*1024, advise_after=1024*1024):
+        """
+        Reimplementation of shutil.copyfileobj that advises the OS to remove
+        parts of the source file from the OS's caches once copied to the
+        destination file.
+
+        Usage profile:
+            * You have a (potentially) large file to copy.
+            * You know you don't need to access the source file once copied.
+            * You're quite likely to access the destination file soon after.
+        """
+        # If we can't access the the fileno then fallback to using shutil.
+        if not hasattr(fsrc, 'fileno'):
+            return shutil.copyfileobj(fsrc, fdst, length)
+        # Calculate the appoximate number of blocks to copy before advising the
+        # OS to drop pages from the cache.
+        advise_after_blocks = int(advise_after/length)
+        # Off we go ...
+        blocks_read = 0
+        while True:
+            data = fsrc.read(length)
+            if not data:
+                break
+            fdst.write(data)
+            blocks_read += 1
+            if not blocks_read % advise_after_blocks:
+                posix_fadvise(fsrc.fileno(), 0, length*blocks_read,
+                              POSIX_FADV_DONTNEED)
+        # One final advise to flush the remaining blocks.
+        posix_fadvise(fsrc.fileno(), 0, 0, POSIX_FADV_DONTNEED)
+
+except ImportError:
+    import warnings
+    warnings.warn("python-fadvise not installed, uploading very large files may adversely affect the OS's file system cache")
+    copyfileobj = shutil.copyfileobj
+
