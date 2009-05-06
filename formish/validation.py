75c7e72bd0e30b6b682afa5c958c8a6d7a1bbec0
Matt Goodall <matt.goodall@gmail.com>
2009-05-06 16:40:06 +0100

    Hide Python 2.6 Exception.message deprecation warning

--- formish/validation.py
+++ formish/validation.py
@@ -191,12 +191,19 @@ class FormishError(Exception):
     The message is not passed on to the Exception base class because it doesn't
     seem to be able to handle unicode at all.
     """
+
     def __init__(self, message, *args):
         Exception.__init__(self, message, *args)
         self.message = message
+
     def __str__(self):
         return self.message
     __unicode__ = __str__
+
+    # Hide Python 2.6 deprecation warnings.
+    def _get_message(self): return self._message
+    def _set_message(self, message): self._message = message
+    message = property(_get_message, _set_message)
 
 
 class FormError(FormishError):
