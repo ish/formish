1617e429f069c808a970b714652d9240e870eea7
Matt Goodall <matt.goodall@gmail.com>
2009-06-12 10:44:27 +0100

    Remove hard-coded paths from standalone testish runner.

--- formish/tests/testish/serve.sh
+++ formish/tests/testish/serve.sh
@@ -1,62 +1,63 @@
-export GITDIR=$HOME/git
-cd $GITDIR/formish/formish/tests/testish/
+# Change to the testish root directory.
+cd `dirname $0`
+
 case "$1" in
   start)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/live.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/live.log \
-                   $GITDIR/formish/formish/tests/testish/live.ini \
+        --pid-file=./log/live.pid \
+        --log-file=./log/live.log \
+                   ./live.ini \
         start
     ;;
   stop)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/live.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/live.log \
-                   $GITDIR/formish/formish/tests/testish/live.ini \
+        --pid-file=./log/live.pid \
+        --log-file=./log/live.log \
+                   ./live.ini \
         stop
     ;;
   restart)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/live.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/live.log \
-                   $GITDIR/formish/formish/tests/testish/live.ini \
+        --pid-file=./log/live.pid \
+        --log-file=./log/live.log \
+                   ./live.ini \
         restart
     ;;
   run)
-    ./run /home/tim/py/bin/paster serve \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/live.log \
-                   $GITDIR/formish/formish/tests/testish/live.ini
+    ./run paster serve \
+        --log-file=./log/live.log \
+                   ./live.ini
     ;;
   start-dev)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/development.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/development.log \
-                   $GITDIR/formish/formish/tests/testish/development.ini \
+        --pid-file=./log/development.pid \
+        --log-file=./log/development.log \
+                   ./development.ini \
         start
     ;;
   stop-dev)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/development.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/development.log \
-                   $GITDIR/formish/formish/tests/testish/development.ini \
+        --pid-file=./log/development.pid \
+        --log-file=./log/development.log \
+                   ./development.ini \
         stop
     ;;
   restart-dev)
-    ./run /home/tim/py/bin/paster serve \
+    ./run paster serve \
         --daemon \
-        --pid-file=$GITDIR/formish/formish/tests/testish/log/development.pid \
-        --log-file=$GITDIR/formish/formish/tests/testish/log/development.log \
-                   $GITDIR/formish/formish/tests/testish/development.ini \
+        --pid-file=./log/development.pid \
+        --log-file=./log/development.log \
+                   ./development.ini \
         restart
     ;;
   run-dev)
-    ./run /home/tim/py/bin/paster serve --reload \
-                   $GITDIR/formish/formish/tests/testish/development.ini
+    ./run paster serve --reload \
+                   ./development.ini
     ;;
   *)
     echo $"Usage: $0 {start|stop|restart|run|start-dev|stop-dev|restart-dev|run-dev}"
